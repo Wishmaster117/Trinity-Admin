bbfe6160c4041f8b14ebbc032618a25fea5af627
Wishmaster117 <140754794+Wishmaster117@users.noreply.github.com>
2025-04-16 17:29:49 +0200

    Update commandes gm.sql

--- tools/commandes gm.sql
+++ tools/commandes gm.sql
@@ -57,7 +57,7 @@ voir les commandes .gobject et voir si on peut mettre une frame de preview
 	
 	('itemmove', 'Syntax: .itemmove #sourceslotid #destinationslotid\r\n\r\nMove an item from slots #sourceslotid to #destinationslotid in your inventory\r\n\r\nNot yet implemented'),
 	
-	voir pour créer une fontion unaura et aura ou équivalent.
+	voir pour créer une fontion .unaura et .aura ou équivalent.
 
 	
 
